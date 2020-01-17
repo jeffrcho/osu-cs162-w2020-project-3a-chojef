4e36af37a8ef0720c8d17cd5774fbcb512440ef7
JeffCho <jeffrcho@gmail.com>
2020-01-17 10:12:55 -0800

    Homework - 3a

---
+++ project-3a-chojef-master/min_max.py
@@ -0,0 +1,26 @@
+# Author: Jeffrey Cho
+# Date: 1/14/2020
+#Description: Write a program that asks the user how many integers they would like to enter. You can
+# assume that this initial input will be an integer >= 1. The program will then prompt the user to
+# enter that many integers. After all the numbers have been entered, the program should display the
+# largest and smallest of those numbers
+
+
+
+print('How many integers would you like to enter?')
+volume = int(input())
+print('Please enter', volume, 'integers.')
+min = int(input())
+max = min
+for i in range(1, volume):
+    num_total = int(input())
+    if num_total < min:
+        min = num_total
+    if num_total > max:
+        max = num_total
+print('min: ', min)
+print('max: ', max)
+
+
+
+
